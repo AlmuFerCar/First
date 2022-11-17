2206a15f54f39f7e0dc602527484d2914a90fea5
Almudena Fernandez <fernandezalmuclases@gmail.com>
2022-11-17 12:41:42 +0100

    cambio imagen fondo pantalla splash

--- app/src/main/java/com/afernandezcar/first/Splash.java
+++ app/src/main/java/com/afernandezcar/first/Splash.java
@@ -34,18 +34,19 @@ public class Splash extends AppCompatActivity {
         thunder.startAnimation(myanim);
 
 
-//        //    Glide for loading girls
-//        ImageView mSea = findViewById(R.id.backView);
-//
-//        Glide.with(this)
-//                .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
-////                .load(R.drawable.girl)
-//                .transition(DrawableTransitionOptions.withCrossFade(100))
-//                .centerCrop()
-////                .diskCacheStrategy(DiskCacheStrategy.ALL)
-//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
-////                .circleCrop()
-//                .into(mSea);
+        //    Glide for loading girls
+        ImageView mSea = findViewById(R.id.backView);
+
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1488655980602-a2d28399df6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80")
+//                .load("https://images.unsplash.com/photo-1594723413117-a07053dd8fe8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80")
+//                .load(R.drawable.hojas)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+//                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+//                .circleCrop()
+                .into(mSea);
 
     }
 
