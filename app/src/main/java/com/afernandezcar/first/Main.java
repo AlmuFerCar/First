2eab6f94d84f4531a0a228fb14432b66710f53ec
Almudena Fernandez <fernandezalmuclases@gmail.com>
2022-11-22 13:42:17 +0100

    cambio toast por snackbar en copy

--- app/src/main/java/com/afernandezcar/first/Main.java
+++ app/src/main/java/com/afernandezcar/first/Main.java
@@ -1,6 +1,7 @@
 package com.afernandezcar.first;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.webkit.WebView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class Main extends AppCompatActivity {
     private WebView miVisorWeb;
@@ -64,27 +67,40 @@ public class Main extends AppCompatActivity {
         switch (item.getItemId())
         {
             case R.id.item1:
-                Toast toast1 =Toast.makeText(this, "Item copied", Toast.LENGTH_LONG);
-                toast1.show();
+//                Toast toast1 =Toast.makeText(this, "Item copied", Toast.LENGTH_LONG);
+//                toast1.show();
 
-                //                final ConstraintLayout mLayout = findViewById(R.id.myMainConstraint);
-//
-//                Snackbar snackbar = Snackbar
-//                        .make(mLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG)
-//                        .setAction("UNDO", new View.OnClickListener() {
-//                            @Override
-//                            public void onClick(View view) {
-//                                Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
-//                                snackbar1.show();
-//                            }
-//                        });
-//
-//                snackbar.show();
+                 final ConstraintLayout mLayout = findViewById(R.id.myMainConstraint);
+
+                Snackbar snackbar = Snackbar
+                        .make(mLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG)
+                        .setAction("UNDO", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
+                                snackbar1.show();
+                            }
+                        });
+
+                snackbar.show();
                 return true;
 
             case R.id.item2:
-                Toast toast2 =Toast.makeText(this, "Downloading item...", Toast.LENGTH_LONG);
-                toast2.show();
+                Toast toast1 =Toast.makeText(this, "Item copied", Toast.LENGTH_LONG);
+                toast1.show();
+//                final ConstraintLayout mLayout2 = findViewById(R.id.myMainConstraint);
+//
+//                Snackbar snackbar2 = Snackbar
+//                        .make(mLayout2, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG)
+//                        .setAction("UNDO", new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//                                Snackbar snackbar3 = Snackbar.make(mLayout2, "Action is restored!", Snackbar.LENGTH_SHORT);
+//                                snackbar3.show();
+//                            }
+//                        });
+//
+//                snackbar2.show();
                 return true;
 
             default:
